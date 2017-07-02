8158acc7a842ee9412fc6b8fc8f8f1e0bd7dc635
Joseph Alai <josephalai@gmail.com>
2017-07-01 18:00:10 -0700

    Initial Commit

---
+++ core/config.php
@@ -0,0 +1,30 @@
+<?php
+	$domain = 'http://diffseo.com';
+
+    // Database Configuration
+	$_db['host'] = 'localhost';
+	$_db['user'] = 'cm3rt';
+	$_db['pass'] = 'Laceration6?';
+	$_db['name'] = 'dating';
+
+	$site_name = 'MatchMe';
+	$meta['keywords'] = '';
+	$meta['description'] = '';
+
+    // Facebook Login Configuration
+	$fb_app_id = '1018459751633376'; 
+	$fb_secret_key = '1d4b541198de9a8aa6a0b4365c1b422c'; 
+
+    // Misc Configuration
+	$minimum_age = '16'; 
+	$email_sender = ''; 
+	
+	// Units of Measurement
+	$unit['height'] = 'in';
+	$unit['weight'] = 'lb';
+	
+	$db = new mysqli($_db['host'], $_db['user'], $_db['pass'], $_db['name']) or die('MySQL Error');
+
+//	error_reporting(-1);
+	error_reporting(0);
+	
